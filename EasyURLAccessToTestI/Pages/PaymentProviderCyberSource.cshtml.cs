314d77abc7880373960f44b97ba9ece367403fdf
Elliott Wilson <elliott.wilson4@jet2.com>
2021-10-05 16:47:11 +0100

    Add project files.

---
+++ EasyURLAccessToTestI/Pages/PaymentProviderCyberSource.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EasyURLAccessToTestI.Pages
+{
+    public class PaymentProviderCyberSourceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
